1f0d6a00a284b3555c1d07dee920548df1dc5aa7
Emilia Urbanek <emurbane@c1r11s3.42warsaw.pl>
2025-07-16 22:30:58 +0200

    help

--- point.h
+++ point.h
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   point.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emurbane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 22:17:36 by emurbane          #+#    #+#             */
+/*   Updated: 2025/07/16 22:20:43 by emurbane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef POINT_H
-#define POINT_H
+# define POINT_H
 
 typedef struct point
 {
-    int obs_y;
-    int visited;
-} point;
+	int	obs_y;
+	int	visited;
+}	t_point;
 
-typedef struct s_rowinfo {
-    int columns;
-    char *add_info;
-} rowinfo;
-
+typedef struct rowinfo
+{
+	int		columns;
+	char	*add_info;
+}	t_rowinfo;
 #endif
